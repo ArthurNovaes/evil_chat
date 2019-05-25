0ec9b1819872ff234d2c3554ce72620dbd46ea9d
¨Arthur¨ <arthurnovaes9@gmail.com>
2019-05-24 22:29:57 -0300

    feature/part-2 Add auth pages
    -
    
    Signed-off-by: ¨Arthur¨ <¨arthurnovaes9@gmail.com¨>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "chat#show"
+
+  get "/login", to: "auth#new"
+  post "/login", to: "auth#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,4 +1,5 @@
 // frontend/packs/application.js
 import "init";
 import "components/page/page";
+import "components/auth-form/auth-form";
 import "./application.css";
