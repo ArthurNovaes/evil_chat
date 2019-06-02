914ac0d80f167216a70536c91ea3f5e33e1711af
¨Arthur¨ <arthurnovaes9@gmail.com>
2019-06-02 15:39:49 -0300

    feature/part-3 Add eslint skip
    -
    
    Signed-off-by: ¨Arthur¨ <¨arthurnovaes9@gmail.com¨>

--- frontend/components/message/message.js
+++ frontend/components/message/message.js
@@ -1,1 +1,22 @@
+import { setCallback } from "client/chat";
+import "components/message/message";
 import "./message.css";
+
+function scrollToBottom(element) {
+  // eslint-disable-next-line
+  element.scrollTop = element.scrollHeight;
+}
+
+const messages = document.querySelector(".js-messages");
+
+if (messages) {
+  const content = messages.querySelector(".js-messages--content");
+
+  scrollToBottom(content);
+
+  setCallback(message => {
+    content.insertAdjacentHTML("beforeend", message);
+
+    scrollToBottom(content);
+  });
+}
